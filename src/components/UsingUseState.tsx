9ddf7c717754ffee26c9d637abae62619896e401
roshan pujara <pujararoshan984@gmail.com>
2022-08-04 19:26:15 +0545

    increment/decrement counter create

---
+++ src/components/UsingUseState.tsx
@@ -0,0 +1,14 @@
+import React,{useState} from "react";
+function UsingUseState(){
+    const [value,setValue] = useState(0);
+    const increment=()=>setValue(value+1);
+    const decrement=()=>setValue(value-1);
+    return(
+        <div>
+            <button onClick={increment}>value increment</button>
+            <h1>{value}</h1>
+            <button onClick={decrement}>value decrement</button>
+        </div>
+    )
+}
+export default UsingUseState
